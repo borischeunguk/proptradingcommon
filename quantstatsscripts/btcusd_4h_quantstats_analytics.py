274ed2062f4fd5ccaa8ae8b95974b2041ee8724f
boris <bo.zhang@icloud.com>
2024-11-30 12:47:56 +0000

    update of quant stats support

--- quantstatsscripts/btcusd_4h_quantstats_analytics.py
+++ quantstatsscripts/btcusd_4h_quantstats_analytics.py
@@ -23,6 +23,6 @@ print(daily_returns_series)
 now = datetime.datetime.now()
 timestamp = now.strftime("%Y%m%d_%H%M%S")  # e.g., 20240203_123456
 product_strategy_name = 'backtrader_macd_trailing_sltp_strategy_btcusdt_4h'
-daily_return_report_name = f"../backtrader_scripts/reports/{product_strategy_name}_daily_return_report"
+daily_return_report_name = f"../backtraderscripts/reports/{product_strategy_name}_daily_return_report"
 daily_return_report_name_timestamp = f'{daily_return_report_name}_{timestamp}.html'
 qs.reports.html(daily_returns_series, "BTC-USD", output=daily_return_report_name_timestamp)
