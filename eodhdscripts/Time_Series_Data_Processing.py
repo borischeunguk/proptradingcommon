6f0f662c0804e1105d9cfd56609b8633fcc9cca3
boris <bo.zhang@icloud.com>
2024-12-14 11:39:22 +0000

    add time series data processing script

--- eodhdscripts/Time_Series_Data_Processing.py
+++ eodhdscripts/Time_Series_Data_Processing.py
@@ -1,23 +1,35 @@
+import datetime
+
 import pandas as pd
+import json
 import os
 
 # Load the JSON file
 
 print(os.getcwd())
-fundamental_file_path = 'AAPL.json'
+product_code = 'AAPL'
+fundamental_file_path = f"{product_code}.json"
 json_file_path = os.getcwd() + '/../resources/fundamental/' + fundamental_file_path
-# data_frame = pd.read_csv(marketdata_file_path, header=None)
-data = pd.read_json(json_file_path)
 
-# Convert to DataFrame
-df = pd.DataFrame(data)
+# Load the JSON data
+with open(json_file_path) as f:
+    data = json.load(f)
 
-# Assuming the JSON has a 'date' column, set it as the index
-df['date'] = pd.to_datetime(df['date'])
-df.set_index('date', inplace=True)
+# Extract Earnings History
+earnings_history = data['Earnings']['History']
 
-# Sort the DataFrame by date
-df.sort_index(inplace=True)
+# Convert the extracted data to a DataFrame
+earnings_df = pd.DataFrame.from_dict(earnings_history, orient='index')
 
-# Display the DataFrame
-print(df)
+# Convert index to datetime
+earnings_df.index = pd.to_datetime(earnings_df.index)
+earnings_df.sort_index(inplace=True)
+
+# Fill missing EPS values with 0 for simplicity
+earnings_df['epsActual'].fillna(0, inplace=True)
+
+# Save the DataFrame to a CSV file
+now = datetime.datetime.now()
+timestamp = now.strftime("%Y%m%d_%H%M%S")  # e.g., 20240203_123456
+csv_file_name = f"results/{product_code}_earnings_history_{timestamp}.csv"
+earnings_df.to_csv(csv_file_name)
