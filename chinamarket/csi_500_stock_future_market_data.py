9e084b7b78b545d9c68207c7866c41f4cbbf5413
boris <bo.zhang@icloud.com>
2025-01-26 20:29:56 +0000

    rename market data files

--- chinamarket/csi_500_stock_future_market_data.py
+++ chinamarket/csi_500_stock_future_market_data.py
@@ -3,16 +3,33 @@ import zipfile
 import pandas as pd
 
 # Define the directory containing the zip files
-data_dir_str = '../resources/'
-market_data_type = 'china_csi_500_stock_future_202410_202411_1min'
+data_dir_str = '../resources/china_csi_500_stock_future_202410_202411_1min'
+
 product_name = 'IC'
+market_data_type = 'china_future_202411_202412_1min_stock_csi_500'
+
 product_name = 'IF'
+market_data_type = 'china_future_202411_202412_1min_stock_csi_300'
+
 product_name = 'IH'
+market_data_type = 'china_future_202411_202412_1min_stock_sse_50'
+
+product_name = 'IM'
+market_data_type = 'china_future_202411_202412_1min_stock_csi_1000'
+
 product_name = 'T'
+market_data_type = 'china_future_202411_202412_1min_bond_10year'
+
 product_name = 'TF'
+market_data_type = 'china_future_202411_202412_1min_bond_5year'
+
 product_name = 'TS'
+market_data_type = 'china_future_202411_202412_1min_bond_2year'
+
 product_name = 'TL'
-zip_dir = os.path.join(os.getcwd(), data_dir_str, market_data_type, product_name)
+market_data_type = 'china_future_202411_202412_1min_bond_30year'
+
+zip_dir = os.path.join(os.getcwd(), data_dir_str, product_name)
 combined_csv_path = os.path.join(zip_dir, 'combined_data.csv')
 
 # Create a list to hold dataframes
@@ -43,7 +60,7 @@ sorted_combined_df = combined_df.sort_values(by='trade_timestamp')
 
 # Save the combined dataframe to a CSV file
 # Define the output file name
-combined_output_file = f"{market_data_type}_{product_name}_combined.csv"
+combined_output_file = f"./combined_data/{market_data_type}_{product_name}_combined.csv"
 sorted_combined_df.to_csv(combined_output_file, index=False)
 print(f"Combined data saved to {combined_output_file}")
 
@@ -56,7 +73,7 @@ for instrument_id, group in grouped:
     sorted_group = group.sort_values(by='trade_timestamp')
 
     # Define the output file name
-    output_file = f"{market_data_type}_{product_name}_{instrument_id}.csv"
+    output_file = f"./instrument_data/{market_data_type}_{product_name}_{instrument_id}.csv"
 
     # Save the sorted group to a CSV file
     sorted_group.to_csv(output_file, index=False)
